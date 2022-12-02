b784ddbf08d6f8096629f0a27dc302f7caaaee3e
Ro Co <roco10000hours@gmail.com>
2022-12-02 10:14:09 +0900

    initial

---
+++ git study/git_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_studyApp.swift
+//  git study
+//
+//  Created by 앙겔로스 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct git_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
